2caff04546ed8f9b0e3965822cf6dc29dbb792c2
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2023-08-13 19:10:01 +0900

    Added Direct Messages

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ CUSTOM_APPS = [
     'wishlists.apps.WishlistsConfig',
     'bookings.apps.BookingsConfig',
     'medias.apps.MediasConfig',
+    'direct_messages.apps.DirectMessagesConfig',
+
 ]
 
 SYSTEM_APPS = [
