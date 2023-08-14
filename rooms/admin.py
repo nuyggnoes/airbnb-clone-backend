c3b0092df027a61e227bcfc4fbaa21e8b0f09902
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2023-08-14 17:47:14 +0900

    Added Total Amenities on list display

--- rooms/admin.py
+++ rooms/admin.py
@@ -10,9 +10,9 @@ class RoomAdmin(admin.ModelAdmin):
         "name",
         "price",
         "kind",
+        "total_amenities",
         "owner",
         "created_at",
-        "updated_at",
     )
 
     list_filter = (
@@ -24,6 +24,9 @@ class RoomAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     )
+
+    def total_amenities(self, room):
+        return room.amenities.count()
 
 
 @admin.register(Amenity)
